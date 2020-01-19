14f3d5f6cd3767453c0abae35329e9b773d9fd27
papion <sjun1319@gmail.com>
2020-01-19 23:47:37 +0900

    2020-01-19 #1

--- src/test/java/com/papion/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/papion/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,10 +1,13 @@
 package com.papion.book.springboot.web.dto;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelloResponseDtoTest {
 
     @Test
     public void 롬복_기능_테스트() {
-
         //given
         String name = "test";
         int amount = 1000;
@@ -15,7 +18,5 @@ public class HelloResponseDtoTest {
         //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
-
     }
-
 }
