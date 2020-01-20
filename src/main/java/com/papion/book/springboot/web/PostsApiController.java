76535e384f0d69d66ab2c1cfbeaf55fc5ffae5cd
papion <sjun1319@gmail.com>
2020-01-20 23:43:19 +0900

    등록 API 진행

---
+++ src/main/java/com/papion/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.papion.book.springboot.web;
+
+import com.papion.book.springboot.service.posts.PostsService;
+import com.papion.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
---
+++ src/main/java/com/papion/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,37 @@
+package com.papion.book.springboot.web.dto;
+
+import com.papion.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    /**
+     * @Builder
+     * 해당 클래스의 빌더 패턴 클래스를 생성
+     * 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+     * @param title
+     * @param content
+     * @param author
+     */
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
