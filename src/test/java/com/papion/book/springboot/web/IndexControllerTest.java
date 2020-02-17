063071c8ad1c46059c988573540658f8783d4eb8
papion <sjun1319@gmail.com>
2020-02-17 23:20:11 +0900

    test 실패하여 수정

--- src/test/java/com/papion/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/papion/book/springboot/web/IndexControllerTest.java
@@ -19,13 +19,10 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩() {
-
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
-
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
-
 }
